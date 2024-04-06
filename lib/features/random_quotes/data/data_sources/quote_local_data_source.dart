2fc2c0a8e11fcf05099fb6a1047f5682c7c1e708
Omar <omarplaystation8888@gmail.com>
2024-04-07 01:45:42 +0200

    ADD QuoteLocalDataSourceImp

--- lib/features/random_quotes/data/data_sources/quote_local_data_source.dart
+++ lib/features/random_quotes/data/data_sources/quote_local_data_source.dart
@@ -1,7 +1,35 @@
+import 'dart:convert';
+
+import 'package:quotes/core/error/exceptions.dart';
 import 'package:quotes/features/random_quotes/data/models/quote_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../../core/utils/app_strings.dart';
 
 abstract class QuoteLocalDataSource {
   Future<QuoteModel> getLastRandomQuote();
 
   Future<void> cacheQuote(QuoteModel quote);
+}
+
+class QuoteLocalDataSourceImp implements QuoteLocalDataSource {
+  QuoteLocalDataSourceImp(this.sharedPreferences);
+
+  final SharedPreferences sharedPreferences;
+
+  @override
+  Future<QuoteModel> getLastRandomQuote() async {
+    final jsonQuote = sharedPreferences.getString(AppStrings.cachedRandomQuote);
+    if (jsonQuote != null) {
+      final localCacheRandomQuote = QuoteModel.fromJson(json.decode(jsonQuote));
+      return localCacheRandomQuote;
+    } else {
+      throw LocalException();
+    }
+  }
+
+  @override
+  Future<void> cacheQuote(QuoteModel quote) {
+    return sharedPreferences.setString(AppStrings.cachedRandomQuote, json.encode(quote));
+  }
 }
