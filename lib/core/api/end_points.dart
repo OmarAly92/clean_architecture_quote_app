e209c8ecddda25fc1695fe71877ae74119223673
Omar <omarplaystation8888@gmail.com>
2024-04-08 23:24:00 +0200

    ADD QuoteRemoteDataSourceImp logic

---
+++ lib/core/api/end_points.dart
@@ -0,0 +1,4 @@
+abstract class EndPoints {
+  static const String baseUrl = 'https://api.quotable.io';
+  static const String randomQuotes = '/random';
+}
--- lib/features/random_quotes/data/data_sources/quote_remote_data_source.dart
+++ lib/features/random_quotes/data/data_sources/quote_remote_data_source.dart
@@ -1,5 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:quotes/core/api/end_points.dart';
+import 'package:quotes/core/error/exceptions.dart';
 import 'package:quotes/features/random_quotes/data/models/quote_model.dart';
 
 abstract class QuoteRemoteDataSource {
   Future<QuoteModel> getRandomQuote();
+}
+
+class QuoteRemoteDataSourceImp implements QuoteRemoteDataSource {
+  const QuoteRemoteDataSourceImp(this.client);
+
+  final http.Client client;
+
+  @override
+  Future<QuoteModel> getRandomQuote() async {
+    final randomQuoteUrl = Uri.parse('${EndPoints.baseUrl}${EndPoints.randomQuotes}');
+    final response = await client.get(
+      randomQuoteUrl,
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      return QuoteModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
