71928e0b31f5febfdc758efdfc1239991fff3d76
Omar <omarplaystation8888@gmail.com>
2024-04-12 00:34:04 +0200

    ADD ChangeLocalUseCase ADD GetSavedLangUseCase

--- lib/features/splash/domain/use_cases/change_local_use_case.dart
+++ lib/features/splash/domain/use_cases/change_local_use_case.dart
@@ -1,1 +1,15 @@
-class ChangeLocalUseCase {}
+import 'package:dartz/dartz.dart';
+import 'package:quotes/core/error/failures.dart';
+import 'package:quotes/core/use_cases/use_case.dart';
+import 'package:quotes/features/splash/domain/repositories/lang_repository.dart';
+
+class ChangeLocalUseCase implements UseCase<bool, String> {
+  ChangeLocalUseCase(this.langRepository);
+
+  final LangRepository langRepository;
+
+  @override
+  Future<Either<Failures, bool>> call(String langCode) async {
+    return await langRepository.changeLang(langCode);
+  }
+}
--- lib/features/splash/domain/use_cases/get_saved_lang_use_case.dart
+++ lib/features/splash/domain/use_cases/get_saved_lang_use_case.dart
@@ -1,1 +1,15 @@
-class GetSavedLangUseCase {}
+import 'package:dartz/dartz.dart';
+import 'package:quotes/core/error/failures.dart';
+import 'package:quotes/core/use_cases/use_case.dart';
+import 'package:quotes/features/splash/domain/repositories/lang_repository.dart';
+
+class GetSavedLangUseCase implements UseCase<String, NoParams> {
+  GetSavedLangUseCase(this.langRepository);
+
+  final LangRepository langRepository;
+
+  @override
+  Future<Either<Failures, String>> call(NoParams noParams) async {
+    return await langRepository.getSavedLang();
+  }
+}
